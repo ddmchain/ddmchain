e134d155c362485cbeb9086824e95bc2a2d33213
ddmchain <17ddmfoundation@gmail.com>
2018-03-31 00:03:21 +0800

    update.

---
+++ consensus/ddmdpos/ddmapi.go
@@ -0,0 +1,88 @@
+package ddmdpos
+
+import (
+	"github.com/ddmchain/go-ddmchain/common"
+	"github.com/ddmchain/go-ddmchain/consensus"
+	"github.com/ddmchain/go-ddmchain/core/types"
+	"github.com/ddmchain/go-ddmchain/rpc"
+)
+
+type DAPI struct {
+	chain  consensus.ChainReader
+	ddmdpos *DDMDPos
+}
+
+func (d *DAPI) GetSnapshot(number *rpc.BlockNumber) (*Snapshot, error) {
+	var header *types.Header
+	if number == nil || *number == rpc.LatestBlockNumber {
+		header = d.chain.CurrentHeader()
+	} else {
+		header = d.chain.GetHeaderByNumber(uint64(number.Int64()))
+	}
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+	return d.ddmdpos.snapshot(d.chain, header.Number.Uint64(), header.Hash(), nil)
+}
+
+func (d *DAPI) GetSnapshotAtHash(hash common.Hash) (*Snapshot, error) {
+	header := d.chain.GetHeaderByHash(hash)
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+	return d.ddmdpos.snapshot(d.chain, header.Number.Uint64(), header.Hash(), nil)
+}
+
+func (d *DAPI) GetSigners(number *rpc.BlockNumber) ([]common.Address, error) {
+	var header *types.Header
+	if number == nil || *number == rpc.LatestBlockNumber {
+		header = d.chain.CurrentHeader()
+	} else {
+		header = d.chain.GetHeaderByNumber(uint64(number.Int64()))
+	}
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+	snap, err := d.ddmdpos.snapshot(d.chain, header.Number.Uint64(), header.Hash(), nil)
+	if err != nil {
+		return nil, err
+	}
+	return snap.signers(), nil
+}
+
+func (d *DAPI) GetSignersAtHash(hash common.Hash) ([]common.Address, error) {
+	header := d.chain.GetHeaderByHash(hash)
+	if header == nil {
+		return nil, errUnknownBlock
+	}
+	snap, err := d.ddmdpos.snapshot(d.chain, header.Number.Uint64(), header.Hash(), nil)
+	if err != nil {
+		return nil, err
+	}
+	return snap.signers(), nil
+}
+
+func (d *DAPI) Proposals() map[common.Address]bool {
+	d.ddmdpos.lock.RLock()
+	defer d.ddmdpos.lock.RUnlock()
+
+	proposals := make(map[common.Address]bool)
+	for address, auth := range d.ddmdpos.proposals {
+		proposals[address] = auth
+	}
+	return proposals
+}
+
+func (d *DAPI) Propose(address common.Address, auth bool) {
+	d.ddmdpos.lock.Lock()
+	defer d.ddmdpos.lock.Unlock()
+
+	d.ddmdpos.proposals[address] = auth
+}
+
+func (d *DAPI) Discard(address common.Address) {
+	d.ddmdpos.lock.Lock()
+	defer d.ddmdpos.lock.Unlock()
+
+	delete(d.ddmdpos.proposals, address)
+}
